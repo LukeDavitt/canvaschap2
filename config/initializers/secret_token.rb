3e3d9243fe90dbdf0b76e62f60ce24ad9cdee225
luke <luke@westlakedesign.com>
2013-05-27 10:33:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CanvasChap2::Application.config.secret_token = 'b6218f1f77cdb42307d09bf663a3de8ca65b4676b7cfb15f5495487b6606690fc653648706edd016b51eaec40da1da8c3414cbb08b895fd86595a1606ac6b254'
---
+++ vendor/assets/javascripts/canvas.js
@@ -0,0 +1,63 @@
+$('document').ready(function() {
+	canvasApp();
+});
+
+
+function canvasApp(){
+	function canvasSupport(){
+		return Modernizr.canvas;
+	}
+
+
+	if(!canvasSupport()){
+		return;
+	}
+	else{
+			var theCanvas = document.getElementById('canvas');
+			var context = theCanvas.getContext('2d');
+	}
+	
+	
+	function drawScreen(){
+		var pie = Math.PI;
+		var x = 50;
+		var y = 100;
+		var width = 40;
+		var height = 40;
+
+		context.setTransform(1,0,0,1,0,0);
+		context.translate(x+0.5*width,y+0.5*height);
+		context.rotate(pie/4);
+		context.fillStyle = 'red';
+		context.fillRect(-.5*width,-.5*height,width,height);
+		
+		context.setTransform(1,0,0,1,0,0);
+		x = 100;
+		y = 100;
+		width = 40;
+		height = 40;
+		context.translate(x+.5*width,y+.5*height);
+		context.rotate(pie/3);
+		context.fillStyle='red';
+		context.fillRect(-.5*width,-.5*height,width,height);
+		
+		context.setTransform(1,0,0,1,0,0);
+		x=150,
+		y=100;
+		context.translate(x+.5*width,y+.5*height);
+		context.rotate(pie/2);
+		context.fillStyle = 'red';
+		context.fillRect(-.5*width,-.5*height,width,height);
+
+		context.setTransform(1,0,0,1,0,0);
+		x=200;
+		context.translate(x+.5*width,y+.5*height);
+		context.rotate(2*pie/3);
+		context.fillStyle = 'red';
+		context.fillRect(-.5*width,-.5*height,width,height);
+
+
+	}
+	drawScreen();
+
+}
