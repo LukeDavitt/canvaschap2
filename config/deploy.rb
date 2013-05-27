9b8dc8fb9deaac3d0688a1418941ae2c9b803476
luke <luke@westlakedesign.com>
2013-05-27 15:23:44 -0400

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,14 @@
+require 'capistrano/ext/multistage'
+set application:'canvaschap2'
+set :scm, :git
+set :repository, "git@github.com:LukeDavitt/canvaschap2.git"
+set :scm_passphrase, ""
+
+set :user, "lukedavitt"
+
+set :stages, ["staging", "production"]
+set :default_stage, "staging"
+
+server "99.22.23.46", :app, :web, :db, :primary => true
+set :deploy_to, "/home/lukedavitt/CanvasChap2"
+
